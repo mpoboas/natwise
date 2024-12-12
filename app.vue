e8b78dfef61d5fd395c48a260c64a74c62523ef3
Miguel Póvoas <1201716@isep.ipp.pt>
2024-12-12 11:13:46 +0000

    Initial Nuxt and Tailwind installation

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container mx-auto mt-16 text-center">
+    <h1 class="text-3xl font-bold text-purple-800">
+      Natwise
+    </h1>
+    <p>Your banking app!</p>
+  </div>
+</template>
